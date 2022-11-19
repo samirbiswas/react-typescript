5b99f26bb8ca00baa94eca1b155a879bf7bd5c8b
samirbiswas <samir99biswas@gmail.com>
2022-11-20 00:59:57 +0600

    all event done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const myComponentStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "200px",
+  border: "3px solid green",
+}
+const Button = {
+  backgroundColor: "#4CAF50",
+  border: "none",
+  color: "white",
+  padding: "15px 32px",
+  textDecoration: "none",
+  display: "inline-block",
+  fontSize: "16px",
+  margin: "4px 2px",
+  cursor: "pointer",
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("click me");
+
+  const changeText = () => {
+
+    setCount(count + 1)
+
+    if (count % 3 === 0) {
+      setText("bang");
+    } else {
+      setText("click me");
+    }
+
+  }
+
+  return <div style={myComponentStyle}>
+    <button style={Button} onClick={changeText}>{text}</button>
+  </div>;
 }
 
 export default App;
